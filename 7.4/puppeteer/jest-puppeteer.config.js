a97367384715ae11e178a5e4e0b9f2c840293021
Evgen7985 <Evgen7985@yandex.ru>
2024-06-27 12:43:25 +0500

    Initial commit

--- 7.4/puppeteer/jest-puppeteer.config.js
+++ 7.4/puppeteer/jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     launch: {
         headless: false,
+        slowMo: 50,
         defaultViewport: null,
         args: ['--start-maximized'] //— используем максимальный размер окна браузера
       },
